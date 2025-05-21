4b3ed6c59c82394bbfe4f1b55b084d3f05289b1c
sabique <sabiqueislam7@gmail.com>
2025-05-21 15:23:53 +0530

    Toast

--- client/reqx/app/layout.tsx
+++ client/reqx/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from 'react-hot-toast';
 import Navbar from "./Pages/Navbar/page";
 
 const inter = Inter({
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={`${inter.variable} antialiased`}
       >
         <Navbar />
+        <Toaster position="top-center" />
         {children}
       </body>
     </html>
